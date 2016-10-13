d59b6bc661bad4a4f4766b34b1521c61d1415db5
Josue <josuem9217@hstat.org>
2016-10-13 09:39:45 +0000

    finnish

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,23 +1,24 @@
-def division
-    
+
+def division(x,y)
+    return x/y
 end
 
-def argue
-  
+def argue(argument)
+  return argument
 end
 
-def greeting
-
+def greeting(greet1,greet2)
+    return greet1 + greet2
 end
 
-def return_a_value
-    
+def return_a_value(value="Nice")
+    return  value
 end
 
-def pizza_party
-    
+def pizza_party(blah="cheese")
+    return blah
 end
 
-def whisper
-    
+def whisper(shhh)
+    return shhh.downcase
 end
